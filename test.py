3d5ec966ecc51e93d913bdc3f3584ee22f8b9a36
TaKj1 <118600431+TaKj1@users.noreply.github.com>
2023-08-25 21:08:11 +0300

    Looking for null Columns in all sheets

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ xls = pd.ExcelFile('/home/genadi/Desktop/all.xlsx', engine='openpyxl')
 for sheet_name in xls.sheet_names:
     df = pd.read_excel(xls, sheet_name=sheet_name, engine='openpyxl')
     
-    # Find columns which have all NaN values
+   
     empty_columns = df.columns[df.isnull().all()].tolist()
 
     if empty_columns:
